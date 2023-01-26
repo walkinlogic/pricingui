b437c8fab5ca2a16613119b1aba2836619431361
Haroon <walkin.logic@gmail.com>
2023-01-26 19:06:32 +0500

    #update nextjs

---
+++ pages/index.js
@@ -0,0 +1,23 @@
+function HomePage() {
+    return (<div class="desk">
+        <div class="pricing">
+            <div class="pricingplan">
+                <div class="boxleft">
+                    <h2 class="premium">premium</h2>
+                    <div class="amount">$329</div>
+                    <div class="billed">billed just once</div>
+                    <div class="btn"><button class="button">Get Started</button></div>
+                </div>
+                <div class="boxright"></div>
+            </div>
+        </div>
+        <div class="header">
+            <div class="main">
+                <h1 class="pricingmain">Simple pricing for your business</h1>
+                <div class="plans">Plans that are carefully crafted to suit your business.</div>
+            </div>
+        </div>
+    </div>)
+}
+
+export default HomePage
